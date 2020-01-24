f2c635af7133145df036332adef8d3c743e11606
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-01-24 19:25:01 -0300

    mensagem em tempo real

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import socketio from 'socket.io-client';
 import { withNavigation } from 'react-navigation';
-import { SafeAreaView, Text, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+import { SafeAreaView, Text, ScrollView, StyleSheet, Image, AsyncStorage, Alert } from 'react-native';
 
 import SpotList from '../components/SpotList';
 
@@ -8,6 +9,18 @@ import logo from '../assets/logo.png';
 
 export default function List() {
     const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.4.5:3333', {
+                query: { user_id }
+            })
+
+            socket.on('booking_response', booking => {
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`)
+            })
+        })
+    }, []);
 
     useEffect(() => {
         AsyncStorage.getItem('techs').then(storagedTechs => {
